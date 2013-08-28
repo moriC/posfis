69a993a63b2bac2406aa4d8561fd63335b5b0f1d
morishita <tomohiro.m0219@gmail.com>
2013-08-28 13:05:19 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Posfis::Application.config.secret_key_base = 'c151f99ecef759dd9a411ef8a52ad2b798f6ee111b952844dbd6584548a24cb187b55a0b38062c7dea50ac4b0fb9c553c31d35693d9cec396f8b42c037fe00e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Posfis::Application.config.session_store :cookie_store, key: '_posfis_session'
