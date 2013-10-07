60edf89d86a81caa246a2b911735e6f443035440
morishita <tomohiro.m0219@gmail.com>
2013-10-07 13:48:44 +0900

    #19 ログイン後のリダイレクト先を変更

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
 
-    def after_update_path_for(manage)
-      user_path(manage)
+    def after_sign_in_path_for(resource)
+      manage_path
     end
 end
