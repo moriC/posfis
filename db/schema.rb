4074ff09732a69f90eadfd81c1d3ac9052624869
morishita <tomohiro.m0219@gmail.com>
2013-11-15 14:09:26 +0900

    ユーザのアバター画像追加用カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131113100837) do
+ActiveRecord::Schema.define(version: 20131115050712) do
 
   create_table "manage_blog_categories", force: true do |t|
     t.string   "category_name"
@@ -55,6 +55,10 @@ ActiveRecord::Schema.define(version: 20131113100837) do
     t.string   "user_address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: true do |t|
