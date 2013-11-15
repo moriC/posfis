a09862fcf1b91005c59c14d27a4dda4686c8048f
morishita <tomohiro.m0219@gmail.com>
2013-11-15 13:22:58 +0900

    画像投稿実装

--- app/models/manage/blog.rb
+++ app/models/manage/blog.rb
@@ -1,4 +1,10 @@
 class Manage::Blog < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :picture
+	has_attached_file :picture, {
+    :styles => {
+      :thumb => ["50x50#", :png],
+      :medium => ["100x100#", :png],
+      :large => ["300x300>", :png]
+    },
+  }
 end
