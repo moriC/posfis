1353c28261a987804ed9bae181d27ad164103ec9
morishita <tomohiro.m0219@gmail.com>
2013-11-18 01:07:07 +0900

    deviseでuserinfoに同時保存できないか奮闘中

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  has_one :userinfo
-  accepts_nested_attributes_for :userinfo
   has_many :manage_blogs
 
+  has_one :userinfo, autosave: true
+  accepts_nested_attributes_for :userinfo
+  
   [:user_name, :user_type, :user_auth, :user_address_number, :user_address].each do |name|
     delegate name, to: :userinfo, allow_nil: true
   end
+
+  
 end
