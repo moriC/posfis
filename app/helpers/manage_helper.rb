b28b512f74f82603cfaa0f9f785d6b58e655d8f4
morishita <tomohiro.m0219@gmail.com>
2013-11-18 13:34:14 +0900

    ログインしているユーザロールを出力

--- app/helpers/manage_helper.rb
+++ app/helpers/manage_helper.rb
@@ -2,4 +2,16 @@ module ManageHelper
 	def format_date(date)
 		p date.strftime("%Y年%m月%d日")
 	end
+
+	def format_user_type(user_type)
+		if user_type == 0
+			p "[システム管理者]"
+		elsif user_type == 1
+			p "[消費者]"
+		elsif user_type == 2
+			p "[生産者]"
+		elsif user_type == 3
+			p "[業者]"
+		end
+	end
 end
