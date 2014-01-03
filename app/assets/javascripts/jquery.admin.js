091d345848e94dd2c4d8657985880f218d59e754
takanashi66 <takanashi66@gmail.com>
2014-01-03 11:36:45 +0900

    jQueryをカプセル化

--- app/assets/javascripts/jquery.admin.js
+++ app/assets/javascripts/jquery.admin.js
@@ -1,65 +1,74 @@
-$(function(){
+(function($) {
 
-	
+ 	jQuery(function($) {
 
-
- 	
-/******************
-* jQuery.masonry
-******************/	
-	var $container = $('.container');
-	$container.imagesLoaded(function(){
-		$container.masonry({
-			itemSelector: '.item',
-			isAnimated: true,
-			duration: 4000,
-			columnWidth: 200,
-			isFitWidth: true
-		});
+		/******************
+		* jQuery.masonry
+		******************/	
+			var $container = $('.container');
+			$container.imagesLoaded(function(){
+				$container.masonry({
+					itemSelector: '.item',
+					isAnimated: true,
+					duration: 4000,
+					columnWidth: 200,
+					isFitWidth: true
+				});
+			});
+		
+		/******************
+		* +-での文字サイズの変更
+		******************/
+		
+			$(".plus").click(function(){
+				$("body").css("font-size","120%");
+				$container.masonry("reload");
+			})
+			
+			$(".minus").click(function(){
+				$("body").css("font-size","13px");
+				$container.masonry("reload");
+			})
+		
+		/******************
+		* ★を押した時の処理
+		******************/
+		
+			$(".star").toggle(function(){
+				$(this).css("color","#FF0");
+				$(this).find(".icon-star-empty").removeClass("icon-star-empty").addClass("icon-star");
+			},function(){
+				$(this).css("color","#858585");
+				$(this).find(".icon-star").removeClass("icon-star").addClass("icon-star-empty");
+			});
+		
+		
+		
+		/******************
+		* 公開非公開の処理
+		******************/
+			
+			$(".status").toggle(function(){
+				$(this).html("非公開");
+			},function(){
+				$(this).html("公開");
+			});
+		
+		
+			$(".dell").click(function(){
+				if(confirm("記事を削除しますか？")){
+					$(this).parents(".box1").hide();
+				}
+			})
+			
+		/*****************
+		* 友達のなる削除
+		*****************/
+		
+		$(".newFriends p").click(function(){
+			$(this).parent().parent().html("友達になりました").fadeOut("slow");
+		})
+		
 	});
-	
 
-/******************
-* 公開非公開の処理
-******************/
-
-	$(".plus").click(function(){
-		$("body").css("font-size","120%");
-		$container.masonry("reload");
-	})
-	
-	$(".minus").click(function(){
-		$("body").css("font-size","13px");
-		$container.masonry("reload");
-	})
-
- 
-
-/******************
-* 公開非公開の処理
-******************
-	
-	$(".status").toggle(function(){
-		$(this).html("非公開");
-	},function(){
-		$(this).html("公開");
-	});
-
-
-	$(".dell").click(function(){
-		if(confirm("記事を削除しますか？")){
-			$(this).parents(".box1").hide();
-		}
-	})
-
-	
-/*****************
-* 友達のなる削除
-*****************/
-
-$(".newFriends p > a").click(function(){
-	$(this).parent().parent().parent().html("友達になりました").fadeOut("slow");
-})
-
-
-})
+})(jQuery);
