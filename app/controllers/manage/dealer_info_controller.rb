1c3041cd4e161c2233bd1dc86b497435183dac09
morishita <tomohiro.m0219@gmail.com>
2014-01-04 15:23:07 +0900

    特定商取引情報表示部分の作成

--- app/controllers/manage/dealer_info_controller.rb
+++ app/controllers/manage/dealer_info_controller.rb
@@ -1,4 +1,5 @@
-class Manage::DealerInfoController < ApplicationController
+class Manage::DealerInfoController < ManageController
 	def index
+		@dealer_info = AsctInfo.where(user_id: current_user.id)
 	end
 end
