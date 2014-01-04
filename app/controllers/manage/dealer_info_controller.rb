a3689073fc6944f320f605b2a94e54932eb20d11
morishita <tomohiro.m0219@gmail.com>
2014-01-04 16:09:07 +0900

    特定商取引情報の新規作成と編集の機能のベース実装

--- app/controllers/manage/dealer_info_controller.rb
+++ app/controllers/manage/dealer_info_controller.rb
@@ -1,13 +1,32 @@
 class Manage::DealerInfoController < ManageController
 	before_action :set_dealer_info, only: [:index, :show, :edit, :update, :destroy]
-	
+
 	def index
-		
+	end
+
+	def new
+		@dealer_info = AsctInfo.new
+	end
+
+	def edit
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
 	end
 
 	private
 		def set_dealer_info
 			@dealer_info = AsctInfo.where(user_id: current_user.id)
 		end
+
+		def edaler_info_params
+      params.require(:asct_infos).permit(:dealer, :manager, :manager_number, :manager_address, :manager_tel, :manager_mail, :additional_costs, :order_method, :pay_method, :pay_limit, :delivered_time, :return_method, :user_id)
+    end
 
 end
